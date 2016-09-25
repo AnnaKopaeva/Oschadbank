8bf660f518409d68c13cfd07c64c868b4be8388c
AnnaKopaeva <an.copava@yandex.ua>
2016-09-25 23:11:38 +0300

    Corected aside

--- api/js/script.js
+++ api/js/script.js
@@ -1,12 +1,35 @@
 
 $('#selectedCurrency').find('option').ready(function(){
-	$(".selected").removeClass("activeCurrency");
-	$(this).addClass("activeCurrency");
+  $(".selected").removeClass("activeCurrency");
+  $(this).addClass("activeCurrency");
 });
 $('.activeCurrency').ready(function(){
 
-	var currency = $(this).attr("currency"); 
-	// $('th[data-currency]').hide(); 
-	$('th[data-currency='+ currency + ']').fadeToggle('2000ms', 'linear'); 
+  var currency = $(this).attr("currency"); 
+  // $('th[data-currency]').hide(); 
+  $('th[data-currency='+ currency + ']').fadeToggle('2000ms', 'linear'); 
 
 });
+
+//slider
+// $('.main-slider').slick({
+//   rtl: true
+// });
+
+// for google map
+function initMap() {
+  var uluru = {lat: 50.483036, lng: 30.377481};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: uluru,
+    zoomControl: false,
+    scaleControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
